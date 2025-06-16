2844a340ddf565f1513cedcdf0f94343100c39b1
Boarders <callan.mcgill@gmail.com>
2025-06-15 23:10:10 -0400

    Return to this

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 #[macro_use]
 extern crate enum_primitive_derive;
 extern crate strum_macros;
+use bit_vec::BitVec;
 
+use logic::propositional::Expr;
 
 pub mod propositional;
 
 fn main() {
-    propositional::syntax::yep();
+    use strum::{EnumCount, IntoEnumIterator};
+    use strum_macros::{EnumCount, EnumIter};
+    #[derive(Debug, Primitive, Clone, PartialEq, Eq, Hash, EnumCount, EnumIter)]
+    enum Foo {
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+    }
+    let foo_size = Foo::COUNT;
+    let bv = BitVec::from_elem(foo_size, true);
+    let ex = Expr::And(
+        Box::new(Expr::Atom(Foo::Red)),
+        Box::new(Expr::Atom(Foo::Blue)),
+    );
+    let not_ex = Expr::Not(Box::new(ex.clone()));
+    println!("{}", ex.eval(&bv));
 }
-
--- src/propositional/syntax.rs
+++ src/propositional/syntax.rs
@@ -1,17 +1,12 @@
 use std::hash::Hash;
 
 use bit_vec::BitVec;
-use num_traits::{ToPrimitive};
-
-
-pub fn yep(){
-    println!("yeeppp!");    
-}
+use num_traits::ToPrimitive;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Expr<T>
 where
-    T : Clone + Eq + Hash,
+    T: Clone + Eq + Hash,
 {
     Top,
     Bot,
@@ -27,45 +22,47 @@ use Expr::*;
 
 impl<T> Expr<T>
 where
-    T : Clone + Eq + Hash + ToPrimitive,
+    T: Clone + Eq + Hash + ToPrimitive,
 {
-    pub fn eval(&self, valuation : &BitVec<u32>) -> bool{
+    pub fn eval(&self, valuation: &BitVec<u32>) -> bool {
         match self {
             Top => true,
             Bot => false,
             Atom(atom) => match valuation.get(T::to_usize(atom).unwrap()) {
                 Some(b) => b,
-                None => panic!("variable not defined",)
-            }
+                None => panic!("variable not defined by valuation",),
+            },
             Not(b) => !b.eval(valuation),
-            And(b1, b2) => b1.eval(valuation) & b2.eval(valuation), 
+            And(b1, b2) => b1.eval(valuation) & b2.eval(valuation),
             Or(b1, b2) => b1.eval(valuation) | b2.eval(valuation),
-                    
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use strum::{EnumCount, IntoEnumIterator};
-    use strum_macros::{EnumCount, EnumIter};
     use super::Expr;
     use bit_vec::BitVec;
-    
+    use strum::{EnumCount, IntoEnumIterator};
+    use strum_macros::{EnumCount, EnumIter};
+
     #[derive(Debug, Primitive, Clone, PartialEq, Eq, Hash, EnumCount, EnumIter)]
     enum Foo {
         Red = 0,
         Blue = 1,
         Green = 2,
     }
-        
+
     #[test]
-    fn eval_1(){
+    fn eval_1() {
         let foo_size = Foo::COUNT;
         let bv = BitVec::from_elem(foo_size, true);
-        let ex = Expr::And(Box::new(Expr::Atom(Foo::Red)), Box::new(Expr::Atom(Foo::Blue)));
+        let ex = Expr::And(
+            Box::new(Expr::Atom(Foo::Red)),
+            Box::new(Expr::Atom(Foo::Blue)),
+        );
         let not_ex = Expr::Not(Box::new(ex.clone()));
-        assert_eq!(true,ex.eval(&bv));
-        assert_eq!(false,not_ex.eval(&bv));
+        assert_eq!(true, ex.eval(&bv));
+        assert_eq!(false, not_ex.eval(&bv));
     }
 }
